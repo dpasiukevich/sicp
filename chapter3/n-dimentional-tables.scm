af8b1e7980cfc276c77fb9bb33669eb3f0d29bb8
Dzmitry Pasiukevich <dpasiukevich@gmail.com>
2019-10-29 12:21:11 +0300

    semiworking assoc table

--- chapter3/n-dimentional-tables.scm
+++ chapter3/n-dimentional-tables.scm
@@ -1,44 +1,54 @@
+(define same-key? equal?)
+
 (define (make-table same-key?)
   (define (assoc key records)
-    (cond ((null? records) false)
+    (cond ((null? records) #f)
+          ((not (pair? records)) #f)
+          ((not (pair? (car records))) #f)
           ((same-key? key (caar records)) (car records))
           (else (assoc key (cdr records)))))
-  (let ((local-table (list '*table*)))
-   (define (lookup key-1 key-2)
-     (let ((subtable
-             (assoc key-1 (cdr local-table))))
-       (if subtable
-           (let ((record
-                   (assoc key-2 (cdr subtable))))
-             (if record (cdr record) false))
-           false)))
-   (define (insert! key-1 key-2 value)
-     (let ((subtable
-             (assoc key-1 (cdr local-table))))
-       (if subtable
-           (let ((record
-                   (assoc key-2 (cdr subtable))))
-             (if record
-                 (set-cdr! record value)
-                 (set-cdr! subtable
-                           (cons (cons key-2 value)
-                                 (cdr subtable)))))
-           (set-cdr! local-table
-                     (cons (list key-1 (cons key-2 value))
-                           (cdr local-table)))))
-     'ok)
+  (define (lookup keys local-table)
+       (define (lookup-helper keys sub-table)
+         (cond ((eq? sub-table #f) sub-table)
+               ((null? keys) (cdr sub-table)) 
+               (else (lookup-helper (cdr keys) (assoc (car keys) (cdr sub-table))))))
+       (if (pair? keys) (lookup-helper keys local-table) #f))
+  (define (insert! keys value local-table)
+    (define (extend-table! table key new-value)
+      (let ((table-elements (cdr table)))
+       (set-cdr! table (cons (cons key new-value)
+                             (if (pair? table-elements) table-elements ())))))
+    (let ((sub-table (assoc (car keys) (cdr local-table))) ; sub-table is a pair (key, value
+          (last-key? (null? (cdr keys))))
+      (if last-key?
+          (if sub-table
+              (set-cdr! sub-table value)
+              (extend-table! local-table (car keys) value))
+          (if sub-table
+              (insert! (cdr keys) value sub-table)
+              (begin (extend-table! local-table (car keys) ())
+                     (insert! (cdr keys) value (cadr local-table))
+                     ))))
+    value)
+  (let ((t (list '*table*)))
    (define (dispatch m)
-     (cond ((eq? m 'lookup-proc) lookup)
-           ((eq? m 'insert-proc!) insert!)
+     (cond ((eq? m 'lookup-proc) (lambda (keys) (lookup keys t)))
+           ((eq? m 'insert-proc!) (lambda (keys value) (insert! keys value t)))
            (else (error "Unknown operation: TABLE" m))))
    dispatch))
 
-(define operation-table (make-table))
+(define operation-table (make-table equal?))
 (define get (operation-table 'lookup-proc))
 (define put (operation-table 'insert-proc!))
 
-
-(put 'x 'y 15)
-(get 'x 'y)
-
-(get 'y 'x)
+(put '(x y) 15)
+(put '(x) 150)
+(get '(x y))
+(get '(k))
+(get '(y x))
+(get '(x))
+(put '(x y z) 22)
+(get '(x y z))
+(get '(x))
+(get '(x y))
+(get '(x y z k))
