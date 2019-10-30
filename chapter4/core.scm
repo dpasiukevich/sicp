b6c980af774293c6c9cd3e6f595c8f9faeaea34b
Dzmitry Pasiukevich <dpasiukevich@gmail.com>
2019-10-30 21:23:02 +0300

    reorganized declarations for metacircular evaluator

--- chapter4/core.scm
+++ chapter4/core.scm
@@ -39,54 +39,45 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
-(define (eval-if exp env)
-  (if (true? (eval (if-predicate exp) env))
-      (eval (if-consequent exp) env)
-      (eval (if-alternative exp) env)))
-
-(define (eval-sequence exps env)
-  (cond ((last-exp? exps) (eval (first-exp exps) env))
-        (else (eval (first-exp exps) env)
-              (eval-sequence (rest-exps exps) env))))
-
-(define (eval-assignment exp env)
-  (set-variable-value! (assignment-variable exp)
-                       (eval (assignment-value exp) env)
-                       env)
-  'ok)
-
-(define (eval-definition exp env)
-  (define-variable! (definition-variable exp)
-                    (eval (definition-value exp) env)
-                    env)
-'ok)
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
 
 (define (self-evaluating? exp)
   (cond ((number? exp) true)
         ((string? exp) true)
         (else false)))
 
+; VARIABLES
 (define (variable? exp) (symbol? exp))
 
+; QUOTATION LISTS
 (define (quoted? exp)
   (tagged-list? exp 'quote))
 (define (text-of-quotation exp) (cadr exp))
 
-(define (tagged-list? exp tag)
-  (if (pair? exp)
-      (eq? (car exp) tag)
-      false))
-
+; ASSIGNMENT
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-value exp) env)
+                       env)
+  'ok)
 (define (assignment? exp)
   (tagged-list? exp 'set!))
 (define (assignment-variable exp) (cadr exp))
 (define (assignment-value exp) (caddr exp))
 
-(define (definition? exp)
-  (tagged-list? exp 'define))
+; DEFINITION
+(define (eval-definition exp env)
+  (define-variable! (definition-variable exp)
+                    (eval (definition-value exp) env)
+                    env)
+'ok)
+(define (definition? exp) (tagged-list? exp 'define))
 (define (definition-variable exp)
   (if (symbol? (cadr exp))
-(cadr exp)
+      (cadr exp)
       (caadr exp)))
 (define (definition-value exp)
   (if (symbol? (cadr exp))
@@ -94,12 +85,14 @@
       (make-lambda (cdadr exp)   ; formal parameters
                    (cddr exp)))) ; body
 
+; LAMBDAS
 (define (lambda? exp) (tagged-list? exp 'lambda))
 (define (lambda-parameters exp) (cadr exp))
 (define (lambda-body exp) (cddr exp))
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 
+; AND
 (define (and? exp) (tagged-list? exp) 'and)
 (define (eval-and exp env)
   (cond ((null? exp) true)
@@ -107,22 +100,15 @@
          (eval-and (cdr exp) env))
         (else false)))
 
+; OR
 (define (or? exp) (tagged-list? exp) 'or)
 (define (eval-and exp env)
   (cond ((null? exp) true)
         ((true? (eval (car exp) env) true))
         (eval-and (cdr exp) env)))
 
-(define (if? exp) (tagged-list? exp 'if))
-(define (if-predicate exp) (cadr exp))
-(define (if-consequent exp) (caddr exp))
-(define (if-alternative exp)
-  (if (not (null? (cdddr exp)))
-      (cadddr exp)
-'false))
-(define (make-if predicate consequent alternative)
-  (list 'if predicate consequent alternative))
-
+; BEGIN AND SEQUENCE OF EXPRESSIONS
+(define (make-begin seq) (cons 'begin seq))
 (define (begin? exp) (tagged-list? exp 'begin))
 (define (begin-actions exp) (cdr exp))
 
@@ -130,27 +116,39 @@
 (define (first-exp seq) (car seq))
 (define (rest-exps seq) (cdr seq))
 
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
 (define (sequence->exp seq)
   (cond ((null? seq) seq)
         ((last-exp? seq) (first-exp seq))
         (else (make-begin seq))))
-(define (make-begin seq) (cons 'begin seq))
 
-(define (application? exp) (pair? exp))
-(define (operator exp) (car exp))
-(define (operands exp) (cdr exp))
-(define (no-operands? ops) (null? ops))
-(define (first-operand ops) (car ops))
-(define (rest-operands ops) (cdr ops))
+; IF
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
 
+; COND
 (define (cond? exp) (tagged-list? exp 'cond))
 (define (cond-clauses exp) (cdr exp))
 (define (cond-else-clause? clause)
   (eq? (cond-predicate clause) 'else))
 (define (cond-predicate clause) (car clause))
 (define (cond-actions clause) (cdr clause))
-(define (cond->if exp)
-  (expand-clauses (cond-clauses exp)))
+(define (cond->if exp) (expand-clauses (cond-clauses exp)))
 (define (expand-clauses clauses)
   (if (null? clauses)
       'false ; no else clause (let ((first (car clauses))
@@ -159,8 +157,16 @@
         (if (cond-else-clause? first)
             (if (null? rest)
                 (sequence->exp (cond-actions first))
-                (error "ELSE clause isn't last -- COND->IF"
-                       clauses))
+                (error "ELSE clause isn't last -- COND->IF" clauses))
             (make-if (cond-predicate first)
-            (sequence->exp (cond-actions first))
-            (expand-clauses rest))))))
+                     (sequence->exp (cond-actions first))
+                     (expand-clauses rest))))))
+
+; APPLICATION - any compound expression that is not defined explicitly
+(define (application? exp) (pair? exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+(define (no-operands? ops) (null? ops))
+(define (first-operand ops) (car ops))
+(define (rest-operands ops) (cdr ops))
+
