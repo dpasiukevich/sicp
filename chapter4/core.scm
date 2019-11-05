58b4f85c7d752ae07fe1b0c229f0b5b19955f04a
Dzmitry Pasiukevich <dpasiukevich@gmail.com>
2019-11-05 22:37:50 +0300

    possibility to add/read/modify variables in env or enclosing envs

--- chapter4/core.scm
+++ chapter4/core.scm
@@ -35,6 +35,73 @@
          (error
           "Unknown procedure type -- APPLY" procedure))))
 
+(define (true? x) (not (eq? x false)))
+(define (false? x) (eq? x false))
+
+(define (make-procedure parameters body env)
+  (list 'procedure parameters body env))
+(define (compound-procedure? p)
+  (tagged-list? p 'procedure))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
+
+(define (enclosing-environment env) (cdr env))
+(define (first-frame env) (car env))
+(define the-empty-environment '())
+
+(define (make-frame variables values)
+  (cons variables values))
+(define (frame-variables frame) (car frame))
+(define (frame-values frame) (cdr frame))
+(define (add-binding-to-frame! var val frame)
+  (set-car! frame (cons var (car frame)))
+  (set-cdr! frame (cons val (cdr frame))))
+
+(define (extend-environment vars vals base-env)
+  (if (= (length vars) (length vals))
+      (cons (make-frame vars vals) base-env)
+      (if (< (length vars) (length vals))
+          (error "Too many arguments supplied" vars vals)
+          (error "Too few arguments supplied" vars vals))))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars)) (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+         (scan (frame-variables frame)
+               (frame-values frame)))))
+  (env-loop env))
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars)) (set-car! vals val))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable: SET!" var)
+        (let ((frame (first-frame env)))
+         (scan (frame-variables frame)
+               (frame-values frame)))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  (let ((frame (first-frame env)))
+   (define (scan vars vals)
+     (cond ((null? vars)
+            (add-binding-to-frame! var val frame))
+           ((eq? var (car vars)) (set-car! vals val))
+           (else (scan (cdr vars) (cdr vals)))))
+   (scan (frame-variables frame) (frame-values frame))))
+
 (define (list-of-values exps env)
   (if (no-operands? exps)
       '()
@@ -50,6 +117,8 @@
   (cond ((number? exp) true)
         ((string? exp) true)
         (else false)))
+
+
 
 ; VARIABLES
 (define (variable? exp) (symbol? exp))
@@ -149,12 +218,16 @@
              ())
        (cons func var-values))))
 
+(define (let*? expr) (tagged-list? expr 'let*))
 (define (let*->nested-lets expr)
   (define (nest variable-list body)
     (if (null? variable-list)
         (make-let variable-list body)
         (make-let (list (car variable-list)) (nest (cdr variable-list) body))))
   (nest (cadr expr) (caddr expr)))
+
+; TODO ADD FOR
+; (for (lambda (cur) (...)) start stop step)
 
 ; IF
 (define (eval-if exp env)
@@ -198,6 +271,7 @@
               (else make-if (cond-predicate first)
                             (sequence->exp (cond-actions first))
                             (expand-clauses rest))))))
+
 
 ; APPLICATION - any compound expression that is not defined explicitly
 (define (application? exp) (pair? exp))
