6823fab142f60067c2d9fd852eefb450543ff076
Dzmitry Pasiukevich <dpasiukevich@gmail.com>
2020-04-28 16:42:00 +0300

    added more test funcs to see compiler generated code

--- chapter5/compiler/compiler_test.scm
+++ chapter5/compiler/compiler_test.scm
@@ -4,8 +4,21 @@
             '(define (factorial n)
                (if (= n 1)
                    1
-                   (* (factorial (- n 1)) n)))
+                   (* n (factorial (- n 1)))))
             'val
             'next))
 
+(define iter-c (compile 
+                 '(define (factorial n)
+                    (define (iter product counter)
+                      (if (> counter n)
+                          product
+                          (iter (* counter product)
+                                (+ counter 1))))
+                    (iter 1 1))
+                 'val
+                 'next))
+
 (pp c)
+
+(pp iter-c)
